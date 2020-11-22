b4fb4abdd878b39e34d323ec5c5fc815f756f507
kiso shuto <1920511h@gsuite.kobe-u.ac.jp>
2020-11-22 17:13:46 +0900

    課題の修正
    4ｑ－1の課題評価が△だったので修正しました

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,4 +1,5 @@
 // テキスト「アニメーション」
+// テキスト「アニメーション」
 let x, y, vx, vy;
 
 function setup(){
@@ -11,7 +12,15 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
+  noStroke()
+  fill(0,0,255)
+
+  translate(x,y)
+  rotate(random(PI/3))
+  translate(-x,-y)
+ star(x,y,50,)
+  x+=5
+  y+=5
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -19,11 +28,13 @@ function draw(){
   if(y > height){ y = 0; }
   if(y < 0){ y = height; }
 }
+function star(cx, cy, r,angle){
 
-function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+
+
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
